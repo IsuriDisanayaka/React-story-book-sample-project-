6796d6f6a5d3f84af034b44f8c0fd34a5677cda3
Isuri Disanayaka <isuriUmeshika1@gmail.com>
2024-06-03 21:55:57 +0530

    ADD styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Button = ({ text }) => {
-  return <div>{text}</div>;
+const Button = ({ text, onClick, style }) => {
+  return (
+    <button onClick={onClick} style={style}>
+      {text}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  style: PropTypes.object,
+};
+
+Button.defaultProps = {
+  onClick: () => {},
+  style: {},
 };
 
 export default Button;
--- src/components/Button.stories.js
+++ src/components/Button.stories.js
@@ -12,3 +12,21 @@ export const Primary = Template.bind({});
 Primary.args = {
   text: "Hello World",
 };
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  text: "Click Me",
+  style: { backgroundColor: "lightblue", color: "darkblue" },
+};
+
+export const WithAction = Template.bind({});
+WithAction.args = {
+  text: "Click Me",
+  onClick: () => alert("Button clicked!"),
+};
+
+export const CustomStyle = Template.bind({});
+CustomStyle.args = {
+  text: "Styled Button",
+  style: { padding: "10px 20px", backgroundColor: "purple", color: "white" },
+};
